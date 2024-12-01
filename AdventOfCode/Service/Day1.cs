f5c9547814d748ccd57594df21f943d7341be89b
stefan lönn <sdlonn@gmail.com>
2024-12-01 13:35:59 +0100

    Added day1

--- AdventOfCode/Service/Day1.cs
+++ AdventOfCode/Service/Day1.cs
@@ -1,11 +1,52 @@
 using AdventOfCode.Service.FileSystem;
 using AdventOfCode.Service.Interface;
+using AdventOfCode.Service.ServiceModels;
 
 namespace AdventOfCode.Service;
 
 public class Day1 : IService
 {
     public string FirstProblem()
+    {
+        var wrapper = GetNumbers();
+
+        wrapper.Left.Sort();
+        wrapper.Right.Sort();
+
+        var diff = 0L;
+
+        for (var i = 0; i < wrapper.Left.Count; i++)
+        {
+            diff += Math.Abs(wrapper.Left[i] - wrapper.Right[i]);
+        }
+
+        return diff.ToString();
+    }
+
+    public string SecondProblem()
+    {
+        var numbers = GetNumbers();
+        Dictionary<int, int> occurences = numbers.Right
+            .GroupBy(x => x)
+            .Select(x => new { key = x.Key, count = x.Count() })
+            .ToDictionary(x => x.key, x => x.count);
+
+        var amount = 0L;
+
+        foreach (var number in numbers.Left)
+        {
+            if (!occurences.ContainsKey(number))
+            {
+                continue;
+            }
+
+            amount += number * occurences[number];
+        }
+
+        return amount.ToString();
+    }
+
+    private Day1Wrapper GetNumbers()
     {
         var inputNumbers =
             FileSystemService.GetFileContentRows(FileSystemService.GetDataFilePath("1", AppSettingConstants.DataFile1));
@@ -19,21 +60,6 @@ public class Day1 : IService
             right.Add(int.Parse(values[1]));
         }
 
-        left.Sort();
-        right.Sort();
-
-        var diff = 0L;
-
-        for (var i = 0; i < left.Count; i++)
-        {
-            diff += Math.Abs(left[i] - right[i]);
-        }
-
-        return diff.ToString();
-    }
-
-    public string SecondProblem()
-    {
-        throw new NotImplementedException();
+        return new Day1Wrapper(left, right);
     }
 }
---
+++ AdventOfCode/Service/ServiceModels/Day1Wrapper.cs
@@ -0,0 +1,13 @@
+﻿namespace AdventOfCode.Service.ServiceModels;
+
+internal class Day1Wrapper
+{
+    public List<int> Left { get; internal set; }
+    public List<int> Right { get; internal set; }
+
+    public Day1Wrapper(List<int> left, List<int> right)
+    {
+        Left = left;
+        Right = right;
+    }
+}

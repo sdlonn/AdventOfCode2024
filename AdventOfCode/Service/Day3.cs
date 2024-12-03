d288b04601e72c84355abb45be259f6ff13798ee
stefan lönn <sdlonn@gmail.com>
2024-12-03 08:04:15 +0100

    Changed to generated regex

--- AdventOfCode/Service/Day3.cs
+++ AdventOfCode/Service/Day3.cs
@@ -4,11 +4,11 @@ using AdventOfCode.Service.Interface;
 
 namespace AdventOfCode.Service;
 
-public class Day3 : IService
+public partial class Day3 : IService
 {
     public string FirstProblem()
     {
-        return new Regex("mul\\((\\d+),(\\d+)\\)")
+        return ValidMultiplyOperations()
             .Matches(GetInput())
             .Cast<Match>()
             .Select(match => long.Parse(match.Groups[1].Value) * long.Parse(match.Groups[2].Value))
@@ -26,4 +26,7 @@ public class Day3 : IService
         return FileSystemService.GetFileContent(
             FileSystemService.GetDataFilePath(3, AppSettingConstants.DataFile1));
     }
+
+    [GeneratedRegex("mul\\((\\d+),(\\d+)\\)")]
+    private static partial Regex ValidMultiplyOperations();
 }

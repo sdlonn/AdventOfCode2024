dfbd77b2054afe1af2e0055e706f8a06dbd5f047
stefan lönn <sdlonn@gmail.com>
2024-12-03 08:00:20 +0100

    Day 3 first problem

--- AdventOfCode/Service/Day3.cs
+++ AdventOfCode/Service/Day3.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+using AdventOfCode.Service.FileSystem;
 using AdventOfCode.Service.Interface;
 
 namespace AdventOfCode.Service;
@@ -6,11 +8,22 @@ public class Day3 : IService
 {
     public string FirstProblem()
     {
-        throw new NotImplementedException();
+        return new Regex("mul\\((\\d+),(\\d+)\\)")
+            .Matches(GetInput())
+            .Cast<Match>()
+            .Select(match => long.Parse(match.Groups[1].Value) * long.Parse(match.Groups[2].Value))
+            .Sum()
+            .ToString();
     }
 
     public string SecondProblem()
     {
         throw new NotImplementedException();
+    }
+
+    public string GetInput()
+    {
+        return FileSystemService.GetFileContent(
+            FileSystemService.GetDataFilePath(3, AppSettingConstants.DataFile1));
     }
 }

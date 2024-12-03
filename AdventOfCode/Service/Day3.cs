8d9df61f559c87d0d99ede026959a90b68aee0e5
stefan lönn <sdlonn@gmail.com>
2024-12-03 19:20:12 +0100

    Day3 update

--- AdventOfCode/Service/Day3.cs
+++ AdventOfCode/Service/Day3.cs
@@ -8,7 +8,7 @@ public partial class Day3 : IService
 {
     public string FirstProblem()
     {
-        return ValidMultiplyOperations()
+        return OperationsRegex()
             .Matches(GetInput())
             .Cast<Match>()
             .Select(match => long.Parse(match.Groups[1].Value) * long.Parse(match.Groups[2].Value))
@@ -18,15 +18,48 @@ public partial class Day3 : IService
 
     public string SecondProblem()
     {
-        throw new NotImplementedException();
+        return GetValuesFromMatches(DoDoNotOperationsRegex()
+            .Matches(GetInput())).ToString();
     }
 
-    public string GetInput()
+    public static string GetInput()
     {
         return FileSystemService.GetFileContent(
             FileSystemService.GetDataFilePath(3, AppSettingConstants.DataFile1));
     }
 
+    private static long GetValuesFromMatches(MatchCollection matches)
+    {
+        var result = 0L;
+        var count = true;
+        foreach (Match item in matches)
+        {
+            switch (item.Value)
+            {
+                case "do()":
+                    count = true;
+                    break;
+                case "don't()":
+                    count = false;
+                    break;
+                default:
+                {
+                    if (count)
+                    {
+                        result += long.Parse(item.Groups[3].Value) * long.Parse(item.Groups[4].Value);
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
     [GeneratedRegex("mul\\((\\d+),(\\d+)\\)")]
-    private static partial Regex ValidMultiplyOperations();
+    private static partial Regex OperationsRegex();
+
+    [GeneratedRegex("(do)\\(\\)|(don't)\\(\\)|mul\\((\\d+),(\\d+)\\)")]
+    private static partial Regex DoDoNotOperationsRegex();
 }
